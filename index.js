afb24485494921001caf961b41601d019c8218b5
Frank <frankjay2017@gmail.com>
2021-05-31 19:11:07 +0400

    configured heroku

--- index.js
+++ index.js
@@ -41,3 +41,7 @@ app.use("/api/customers", customerRoutes)
 app.use("/api/categories", categoryRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!")
+})
